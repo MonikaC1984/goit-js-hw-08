11c45f39dbfdb65dccef58c93ad3abe051a1853c
MonikaC1984 <113378661+MonikaC1984@users.noreply.github.com>
2023-03-06 22:49:06 +0100

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,8 @@
+import Player from '@vimeo/player';
 
+const player = new Player('vimeo-player');
+
+player.on('timeupdate', function (data) {
+  // localStorage.setItem('videoplayer-current-time', data);
+  console.log(data);
+});
