d9f0ed7f7108edb3775662610daaaf365208666a
MonikaC1984 <113378661+MonikaC1984@users.noreply.github.com>
2023-03-06 15:52:32 +0100

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
+// Opisany w dokumentacji
+import SimpleLightbox from 'simplelightbox';
+// Dodatkowy import stylów
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const photoPalette = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    img => `<div class="gallery__item">
+    <a class="gallery__link" href="${img.original}">
+      <img
+        class="gallery__image"
+        src="${img.preview}"
+            data-source="${img.original}"
+             alt="${img.description}"
+      />
+    </a>
+  </div>`
+  )
+  .join('');
+
+photoPalette.insertAdjacentHTML('afterbegin', markup);
+let lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
