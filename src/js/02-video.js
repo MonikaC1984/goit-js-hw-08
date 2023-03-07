d8145eae933bb96c2afbc3402014258085af1658
MonikaC1984 <113378661+MonikaC1984@users.noreply.github.com>
2023-03-07 10:10:56 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,16 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash/throttle';
 
 const player = new Player('vimeo-player');
 
-player.on('timeupdate', function (data) {
-  // localStorage.setItem('videoplayer-current-time', data);
-  console.log(data);
-});
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+    console.log(data);
+  }, 1000)
+);
+
+player.setCurrentTime(
+  JSON.parse(localStorage.getItem('videoplayer-current-time'))
+);
