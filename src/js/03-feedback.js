4452f24dc68ab38547b662d8cb4ce16b7c8f3a08
MonikaC1984 <113378661+MonikaC1984@users.noreply.github.com>
2023-03-07 21:10:07 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash/throttle';
 
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', saveMsg);
+
+throttle(function saveMsg(event) {
+  event.preventDefault();
+
+  const dataInput = {
+    email: event.currentTarget.email.value,
+    message: event.currentTarget.message.value,
+  };
+
+  console.log(dataInput);
+  localStorage.setItem('feedback-form-state', JSON.stringify(dataInput));
+}, 500);
+
+if (JSON.parse(localStorage.getItem('dataInput')) !== '') {
+  const formElement = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+  form.elements.email.value = '' || formElement.email;
+  form.elements.message.value = '' || formElement.message;
+}
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  localStorage.removeItem('feedback-form-state');
+  form.elements.email.value = '';
+  form.elements.message.value = '';
+});
