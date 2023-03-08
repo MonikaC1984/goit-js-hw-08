9d7a04a3199c31ae5d5e4c8f2f1a44f7dfc5422a
MonikaC1984 <113378661+MonikaC1984@users.noreply.github.com>
2023-03-08 16:50:25 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,32 +1,31 @@
-import throttle from 'lodash/throttle';
+import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 
-form.addEventListener('input', saveMsg);
-
-throttle(function saveMsg(event) {
+function saveMsg(event) {
   event.preventDefault();
-
   const dataInput = {
     email: event.currentTarget.email.value,
     message: event.currentTarget.message.value,
   };
-
   console.log(dataInput);
-  localStorage.setItem('feedback-form-state', JSON.stringify(dataInput));
-}, 500);
 
-if (JSON.parse(localStorage.getItem('dataInput')) !== '') {
+  localStorage.setItem('feedback-form-state', JSON.stringify(dataInput));
+}
+
+form.addEventListener('input', throttle(saveMsg, 500));
+
+if (JSON.parse(localStorage.getItem('feedback-form-state')) !== '') {
   const formElement = JSON.parse(localStorage.getItem('feedback-form-state'));
 
-  form.elements.email.value = '' || formElement.email;
-  form.elements.message.value = '' || formElement.message;
+  form.email.value = formElement.email || '';
+  form.message.value = formElement.message || '';
 }
 
 form.addEventListener('submit', event => {
   event.preventDefault();
 
   localStorage.removeItem('feedback-form-state');
-  form.elements.email.value = '';
-  form.elements.message.value = '';
+  form.email.value = '';
+  form.message.value = '';
 });
